8f9fcdaaeb363cd12522633f31e1880544f926bb
Lars <lars@rescomp-14-290435.stanford.edu>
2014-11-13 02:02:15 -0800

    user query expanded -- but takes forever to run as of now :(

--- SQL/141112_UsersWithEditsAndTalk.sql
+++ SQL/141112_UsersWithEditsAndTalk.sql
@@ -1,6 +1,12 @@
-select * 
+select user_name, user_registration, user_editcount, page_counter, count(*) as count_talkrevisions
 from user 
+join page on page_title = user_name
+left join revision_userindex on rev_page = page_id
+
 where year(user_registration) = 2013
 and user_editcount >= 1
+and page_namespace=3 # User talk pages
+group by user_name, user_registration, user_editcount, page_counter
+order by count_talkrevisions desc
 
-limit 10
+limit 100
