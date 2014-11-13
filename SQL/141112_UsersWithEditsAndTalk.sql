dcd4c3fbf872072472cdbd6ca8834b203ed730b0
Lars <lars@rescomp-14-290435.stanford.edu>
2014-11-13 11:03:13 -0800

    optimized query a bit

--- SQL/141112_UsersWithEditsAndTalk.sql
+++ SQL/141112_UsersWithEditsAndTalk.sql
@@ -3,10 +3,8 @@ from user
 join page on page_title = user_name
 left join revision_userindex on rev_page = page_id
 
-where year(user_registration) = 2013
+where user_registration >= 20130101000000 and user_registration < 20140101000000
 and user_editcount >= 1
 and page_namespace=3 # User talk pages
 group by user_name, user_registration, user_editcount, page_counter
 order by count_talkrevisions desc
-
-limit 100
