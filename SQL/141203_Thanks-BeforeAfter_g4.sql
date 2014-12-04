e7af647e3193f5e34bb2c3ecb8045ae7e205220f
Lars <lars@rescomp-14-290435.stanford.edu>
2014-12-04 02:28:29 -0800

    thanks analysis

---
+++ SQL/141203_Thanks-BeforeAfter_g4.sql
@@ -0,0 +1,30 @@
+# Do a comparison of number of edits 30 days before and after official 
+# introduction of "thanks" feature (05/30/2013)
+#
+# Group 1: thanks received in month after launch, activity before launch => exists, timestamp < 05/30
+# Group 2: thanks received in month after launch, activity after launch => exists, timestamp > 05/30
+# Group 3: no thanks received in month after launch, activity before launch => not exists, timestamp < 05/30
+# Group 4: no thanks received in month after launch, activity after launch => not exists, timestamp > 05/30
+# 
+# Created: Lars Roemheld, 12/03/14
+#
+
+select user_id, count(rev_id) as num_edits
+from 
+user
+left join (
+		select rev_id, rev_user from revision_userindex 
+		where rev_timestamp >= 20130530000000 and rev_timestamp < 20130630000000
+			and rev_deleted = 0
+		) as temp_revs
+on rev_user = user_id
+
+where not exists (select * from logging_logindex 
+		where log_timestamp >= 20130530000000 and log_timestamp < 20130630000000
+			and log_type='thanks'
+			and log_deleted = 0
+			and log_title = user_name
+		) 
+and user_editcount >= 1
+
+group by user_id
--- preprocess/text_features_joinUserId.R
+++ preprocess/text_features_joinUserId.R
@@ -2,7 +2,7 @@
 # Based on user_to username.
 
 setwd('/Users/lars/Documents/OfficeFiles/Uni/2014F_MS&E 331 Computational Social Science/project');
-textfeatures = read.delim("text_features_new.tsv", sep="\t", header=TRUE)
+textfeatures = read.delim("text_features_new.tsv", sep="\t", quote="", header=TRUE)
 
 users = read.delim("all-users-2013.tsv", sep="\t", header=TRUE)
 
