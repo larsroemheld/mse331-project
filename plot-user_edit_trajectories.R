32d7588b6079e77f7dd1878ba0c6caa35461f47c
Lars <lars@DN0a2254ea.SUNet>
2015-03-02 01:50:22 -0800

    First working code: line following and ball-requests are working. problems: line following responds slowly, in S-curve manner. Desired: faster reaction, leading in a straight path on the tape.

--- plot-user_edit_trajectories.R
+++ plot-user_edit_trajectories.R
@@ -18,7 +18,7 @@ dt2 = dt2[order(-num_edits)]
 # Make sure user_id appears as factor label, not numerical value
 data$user_id = as.factor(data$user_id)
 
-theme_set(theme_gray(base_size = 24))
+theme_set(theme_gray(base_size = 14))
 # Plot a hand-selected overview of some rockstar editors
 plot_data = data[data$user_id == dt2$user_id[1] | data$user_id == dt2$user_id[5] | data$user_id == dt2$user_id[10] | data$user_id == dt2$user_id[2]]
 overview <- qplot(user_age, edits, data=plot_data, color=user_id, geom="line", main="Activity of 4 Rockstar Editors", xlab = "Weeks since registration", ylab="Edits in Week", size=I(1))
@@ -27,7 +27,7 @@ overview
 
 # Plot a fancy visualization of \tau - neighborhoods and events
 plot_data = data[data$user_id == dt2$user_id[5]]
-p <- qplot(user_age, edits, data=plot_data, color=user_id, geom="line", main="Sample τ-Neighborhood & Events", xlab = "Weeks since registration", ylab="Edits in Week", size=I(1))
+p <- qplot(user_age, edits, data=plot_data, color=user_id, geom="line", main="Sample: t-Neighborhood & Events", xlab = "Weeks since registration", ylab="Edits in Week", size=I(1))
 p <- p + geom_ribbon(data = data[data$user_id == dt2$user_id[5]], aes(ymin=n1_numWeekEdits-std_dev, ymax=n1_numWeekEdits+std_dev), alpha=0.2)
 
 lines = data[data$user_id == dt2$user_id[5]]
@@ -35,10 +35,10 @@ lines$changeevent = abs(lines$edits - lines$n1_numWeekEdits) > lines$std_dev
 for (l in 1:nrow(lines)) {
   if (lines[l]$changeevent) {
 #    p <- p + geom_vline(x = lines[l]$user_age, color = "blue", size = I(1))
-    p <- p + geom_point(x = lines[l]$user_age, y = lines[l]$edits, color="#0072B2", size = I(5))
+    p <- p + geom_point(x = lines[l]$user_age, y = lines[l]$edits, color="#0072B2", size = I(3))
   }
 }
-p <- p + theme_bw(base_size = 24)
+p <- p + theme_bw(base_size = 14)
 #p <- p + theme_grey() 
 p <- p + theme(legend.position="none")
 p
